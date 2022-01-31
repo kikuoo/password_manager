ba5ef6e6afa1773aa59ff997097b2f8b479e7cac
kikuoo <kikuoo505@gmail.com>
2022-02-01 00:09:30 +0900

    220131

--- lib/Pass.dart
+++ lib/Pass.dart
@@ -1,3 +1,4 @@
+
 import package:hive/hive.dart;
 part 'Pass.g.dart';
 
---
+++ lib/PassList.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/cupertino.dart';
+
+class PassList extends StatelessWidget{
+  final List<Pass> pass;
+  const PassList(this.pass);
+  Widget build(BuildContext context){
+    return ListView.builder(
+        itemCount.pass.length,
+        itemBuilder(BuildContext context,int index){
+          var pas = pass[index];
+          return _buildPass(pas);
+
+
+    }
+        )
+  }
+
+}
