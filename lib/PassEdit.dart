1a35f28feccbb7c37cc333b9ef945beac50e10b5
kikuoo <kikuoo505@gmail.com>
2022-04-05 00:15:43 +0900

    220331-2 editページ作成中

--- lib/PassEdit.dart
+++ lib/PassEdit.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:hive/hive.dart';
-
 import 'Pass.dart';
 
-class PassEdit extends StatelessWidget{
+class PassEdit extends State {
+
   final int keyed;
+
   PassEdit(this.keyed);
+
 
   @override
   Widget build(BuildContext context) {
@@ -13,24 +15,33 @@ class PassEdit extends StatelessWidget{
     Pass pass = box.get(keyed);
     return Scaffold(
         appBar: AppBar(
-        title: Text('リスト追加'),
-    ),
-    body: Container(
-    padding: EdgeInsets.all(64),
-    child: Column(
-    mainAxisAlignment: MainAxisAlignment.center,
+          title: Text('リスト追加'),
+        ),
+        body: Container(
+            padding: EdgeInsets.all(64),
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
 
-    children: <Widget>[
-    const SizedBox(height: 8),
-    TextField( controller: TextEditingController
-      (text:pass.id),
-    ),]
-    ))
+                children: <Widget>[
+                  const SizedBox(height: 8),
+                  TextField(controller: TextEditingController
+                    (text: pass.id),onChanged: (String value){
+                    setState(() {
+                       = value;
+
+                    });
+                  },
+
+                  ),
+                ]
+            ))
 
     );
   }
 
 }
+
+
 
 
 
